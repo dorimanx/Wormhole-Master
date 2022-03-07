4c2b2c38b88186ab835cd2670c7c1dac074a10cc
Yuri <Yuri@bynet.co.il>
2022-03-07 19:53:31 +0300

    fancy color picker
    disabling color picker and destination editor while no gates selected
    fixed config flickering
    
    Commit from:
    https://github.com/zznty/Wormhole/commit/c1572bf71c5c6cd21e1e8a522bcb35731fa21840

--- Wormhole/Utilities.cs
+++ Wormhole/Utilities.cs
@@ -300,4 +300,11 @@ namespace Wormhole
             }
         }
     }
+    
+    public static class ColorConverterExtensions
+    {
+        public static string ToHexString(this System.Windows.Media.Color c) => $"#{c.R:X2}{c.G:X2}{c.B:X2}";
+
+        public static string ToRgbString(this System.Windows.Media.Color c) => $"RGB({c.R}, {c.G}, {c.B})";
+    }
 }
