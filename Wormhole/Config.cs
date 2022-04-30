31484638184fe81c00d0a0d8b6e92709729b7e9f
Yuri <Yuri@bynet.co.il>
2022-04-30 23:30:17 +0300

    added config option to toggle server query
    https://github.com/zznty/Wormhole/commit/2272b1dbeedf3bfb0dbc5d5a864903e87a963f67

--- Wormhole/Config.cs
+++ Wormhole/Config.cs
@@ -61,5 +61,8 @@ namespace Wormhole
 
         [Display(Name = "Max Arrival Distance From Gate", Description = "Maximum spawn distance (metters) from gate when arriving from other server, must be more then MIN!, Server to Server Only.")]
         public int MaxDistance { get; set; } = 5;
+
+        [Display(Name = "Check Is Target Server Full", Description = "Check if target server is full on jump")]
+        public bool CheckIsTargetServerFull { get; set; } = true;
     }
 }
